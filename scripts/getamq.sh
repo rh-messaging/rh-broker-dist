02dc49d022f2acf760b74659615bf2d3fa90df0e
andytaylor <andy.tayls67@gmail.com>
2019-01-28 13:55:11 +0000

    update python call

--- scripts/getamq.sh
+++ scripts/getamq.sh
@@ -2,4 +2,4 @@
 
 export json=`curl https://stagger-rhm.cloud.paas.upshift.redhat.com/api/repos/rh-broker-dist/branches/master/tags/untested`
 
-python3 ./scripts/parseJson.py $json
+python ./scripts/parseJson.py $json
