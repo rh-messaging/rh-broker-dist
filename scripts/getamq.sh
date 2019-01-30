002f90b650f6689b096998b453a6b45a1bfd5814
andytaylor <andy.tayls67@gmail.com>
2019-01-30 13:13:29 +0000

    add insecure flag to curl

--- scripts/getamq.sh
+++ scripts/getamq.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export json=`curl https://stagger-rhm.cloud.paas.upshift.redhat.com/api/repos/rh-broker-dist/branches/master/tags/untested`
+export json=`curl --insecure https://stagger-rhm.cloud.paas.upshift.redhat.com/api/repos/rh-broker-dist/branches/master/tags/untested`
 
 python ./scripts/parseJson.py $json
